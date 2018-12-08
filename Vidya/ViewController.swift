c39e3082147ffebe909d4b32403f05966f71977b
vidyaAAM <45511958+vidyaAAM@users.noreply.github.com>
2018-12-08 13:26:31 +0530

    Update ViewController.swift
    Second commit from git

--- Vidya/ViewController.swift
+++ Vidya/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       print("Vidya ")
+       print("Vidya Mahadik")
     }
 
     override func didReceiveMemoryWarning() {
