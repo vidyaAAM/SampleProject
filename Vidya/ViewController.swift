d8bd42948b5fe5ead12d4a3ce655c11d97b49805
NAYAN MANDRE <nayanmandre.aam@gmail.com>
2018-12-08 14:20:32 +0530

    Just commit

--- Vidya/ViewController.swift
+++ Vidya/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       print("Vidya Mahadik")
+       print("Vidya Mahadik BE")
     }
 
     override func didReceiveMemoryWarning() {
